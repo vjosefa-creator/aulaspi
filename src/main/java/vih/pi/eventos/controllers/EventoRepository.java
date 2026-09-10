bd85157eee95029631edc0f163e92000831b93f1
Josefa Vitória Moura Dantas <20241204010008@ifrn.local>
2026-09-10 15:40:52 -0300

    Criando a listagem de eventos

---
+++ src/main/java/vih/pi/eventos/controllers/EventoRepository.java
@@ -0,0 +1,12 @@
+package vih.pi.eventos.controllers;
+
+import vih.pi.eventos.models.Evento;
+
+public class EventoRepository {
+
+	public save(Evento evento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/vih/pi/eventos/controllers/EventosController.java
+++ src/main/java/vih/pi/eventos/controllers/EventosController.java
@@ -1,14 +1,30 @@
 package vih.pi.eventos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import vih.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
 	
+	@Autowired
+	private EventoRepository er;
+	
 	@RequestMapping("/eventos/form")
 	public String form() {
-		return "formEvento";
+		return "eventos/formEvento";
+	}
+	
+	@PostMapping("/eventos") 
+	public String adicionar(Evento evento) {
+		
+		System.out.println(evento);
+		er.save(evento);
+			
+	return "eventos/evento-adicionado";
 	}
 
 }
